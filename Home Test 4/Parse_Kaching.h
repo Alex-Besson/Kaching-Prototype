83dcc2b79d3140f9e6770f684706a8143e41515a
Miwand <miwand.najafe@gmail.com>
2015-10-04 21:54:55 -0400

    Added ParseKeys

---
+++ Home Test 4/Parse_Kaching.h
@@ -0,0 +1,13 @@
+//
+//  Parse_Kaching.h
+//  Home Test 4
+//
+//  Created by Miwand Najafe on 2015-10-04.
+//  Copyright © 2015 Alexander Besson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Parse_Kaching : NSObject
+
+@end
