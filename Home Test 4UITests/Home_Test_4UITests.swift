89fdd1cbb608f88955448d89f63b72aa6f3e82c2
ReissZ <reisszurbyk@live.ca>
2015-10-26 21:18:14 -0400

    Fixing merging error

--- Home Test 4UITests/Home_Test_4UITests.swift
+++ Home Test 4UITests/Home_Test_4UITests.swift
@@ -23,14 +23,31 @@ class Home_Test_4UITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
+    func testSimple() {
+
+        let app = XCUIApplication()
+        app.tables.progressIndicators["90%"].tap()
+        app.buttons["Commit!"].tap()
+        app.textFields["Username"].tap()
+        app.buttons["Login"].tap()
+        
+
+    }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        app.tables.progressIndicators["90%"].tap()
+        app.buttons["Commit!"].tap()
+        app.textFields["Username"].tap()
+        app.buttons["Login"].tap()
     }
     
 }
